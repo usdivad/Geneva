3397f58e89751dd8713eeff513d81cb6492b26f0
David Su <david.d.su@gmail.com>
2015-09-03 03:13:45 -0400

    fix bugs, implement populate, todo: mutate

--- js/geneva.js
+++ js/geneva.js
@@ -4,6 +4,8 @@ var Geneva = Geneva || {};
 Geneva.crossoverRate = 0.7;
 Geneva.mutationRate = 0.2;
 Geneva.root = 440;
+Geneva.DEFAULT_CHROMOSOMES = 10;
+Geneva.DEFAULT_NOTES = 10
 Geneva.phraseMutationMethods = [Geneva.invert, Geneva.transpose, Geneva.retrograde];
 Geneva.noteMutationMethods = [Geneva.addNote, Geneva.removeNote, Geneva.scaleNoteRhythm];
 Geneva.pitchMutationMethods = [];
@@ -13,9 +15,9 @@ Geneva.rhythmMutationMethods = [];
 // Session class
 Geneva.Session = function(tunings) {
     if (tunings == undefined) {
-        tunings = Geneva.scale.shierlu;
+        tunings = Geneva.tunings.shierlu;
     }
-    var scaleMatrix = Geneva.scaleMatrices.choose();
+    var scaleMatrix = Geneva.scaleMatrices[Object.keys(Geneva.scaleMatrices).choose()];
     var scale = [];
     for (var i=0; i<scaleMatrix.length; i++) {
         if (scaleMatrix[i] == 1) {
@@ -29,17 +31,17 @@ Geneva.Session = function(tunings) {
 Geneva.Session.prototype = {
     constructor: Geneva.Session,
 
-    populate: function(n) {
-        for (i=0; i<n; i++) {
-            var notes = this.generateNotes();
+    populate: function(numChromosomes, numNotes) {
+        for (i=0; i<numChromosomes; i++) {
+            var notes = this.generateNotes(numNotes);
             this.chromosomes[i] = new Geneva.Chromosome(notes);
         }
     },
 
-    generateNotes: function(n, mode="random") {
+    generateNotes: function(n, mode) {
         var notes = [];
 
-        if (mode == "random") {
+        if (mode == "random" || mode == undefined) {
             for (var i=0; i<n; i++) {
                 var note = this.scale[Math.floor(Math.random()*this.scale.length)];
                 notes.push(new Geneva.Note(note, 1));
@@ -103,13 +105,22 @@ Geneva.Chromosome.prototype = {
                 }
             }
         }
-    }
+    },
 
     invert: function(root) {
         for (var i=0; i<this.notes.length; i++) {
             var pitch = this.notes[i].pitch;
             // invert around root
         }
+    },
+
+    toString: function() {
+        var str = "";
+        for (var i=0; i<this.notes.length; i++) {
+            var note = this.notes[i];
+            str += "(p:" + note.pitch + ", r:" + note.rhythm + ")\n";
+        }
+        return str;
     }
 };
 
@@ -128,7 +139,7 @@ Geneva.Note.prototype = {
     mutateRhythm: function() {
         var mutationMethod = Geneva.rhythmMutationMethods.choose(); //this.rhythmMutationMethods?
         return mutationMethod(this);
-    }
+    },
 
     transpose: function(step, scale) {
         if (typeof(scale) === undefined) {
@@ -161,6 +172,7 @@ Geneva.scaleMatrices = {
 
 Geneva.tunings = {
     shierlu: [1, 2187/2048, 9/8, 1968/1630, 81/64, 1771/1311, 729/512, 3/2, 6561/4096, 27/16, 5905/3277, 243/128],
+    guqin: [1, 3/4, 2/3, 1/2, 1/3, 1/4],
     just: [1, 9/8, 5/4, 4/3, 3/2, 5/3, 15/8, 2/1]
 };
 
@@ -168,4 +180,8 @@ Geneva.tunings = {
 /*
  * TESTING
  */
-var session = new Geneva.Session(Geneva.tunings.shierlu);
+var session = new Geneva.Session();
+session.populate(Geneva.DEFAULT_CHROMOSOMES, Geneva.DEFAULT_NOTES);
+var c0 = session.chromosomes[0];
+console.log(c0);
+c0.mutate();
